f62219a70e8de9a86bb89e57c5123c1a8c2f0302
Oleg Pashkovsky <oleg@pashkovsky.me>
2024-05-02 12:35:03 +0300

    Fix typo

--- src/Services/RemoteAuthService.php
+++ src/Services/RemoteAuthService.php
@@ -2,13 +2,22 @@
 
 namespace Kwidoo\RemoteUser\Services;
 
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Http\Client\ConnectionException;
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Http;
 use Kwidoo\RemoteUser\Contracts\AuthService;
 use Kwidoo\RemoteUser\Exceptions\RemoteAuthorizationException;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class RemoteAuthService implements AuthService
 {
+    /**
+     * Get access token from remote Laravel Passport server. This will obtain client credentials grant.
+     *
+     * @return string
+     */
     public function getAccessToken(): string
     {
         return Cache::remember('access_token', 3000, function () {
@@ -27,6 +36,19 @@ class RemoteAuthService implements AuthService
         });
     }
 
+    /**
+     * Retrieve user data from the remote service. Since this relies on an external service, the
+     * $identifier is fetched from the frontend request header: config('iam.token_header', 'X-IAM-Token').
+     * In the console or where the request header is not available, the $identifier can be passed as a parameter.
+     *
+     * @param mixed $identifier
+     * @param mixed $retry
+     * @return array
+     * @throws HttpException
+     * @throws NotFoundHttpException
+     * @throws HttpResponseException
+     * @throws ConnectionException
+     */
     public function retrieveFromApi($identifier = null, $retry = null)
     {
         $token = $this->getAccessToken();
